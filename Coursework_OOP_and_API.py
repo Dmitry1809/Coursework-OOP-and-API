6d2cb24401713d5ec5d2027d671e59b30504e97f
Dmitrij Bakeev <dmitrij.bakeev@gmail.com>
2025-07-22 17:16:30 +0300

    with functions

--- Coursework_OOP_and_API.py
+++ Coursework_OOP_and_API.py
@@ -1,33 +1,40 @@
 from fileinput import filename
-
 import requests
+import json
+
+def loading_cat_photo (text, url):
+    response = requests.get(url)
+    print(response.headers)
+    content_length = response.headers['Content-Length']
+    content_type = response.headers['Content-Type']
+    date = response.headers['Date']
+    with open('goodbye.jpg', 'wb') as f:
+         f.write(response.content)
+
+    data_about_image = {
+    'Content-Length': content_length,
+    'Content-Type': content_type,
+    'Date': date
+    }
+
+    with open("data_about_image.json", "w") as json_file:
+        json.dump(data_about_image, json_file, indent=4)
 
 text = 'Всем пока'
 url = f'https://cataas.com//cat/says/{text}'
-response = requests.get(url)
-print(response.headers)
-content_length = response.headers['Content-Length']
-content_type = response.headers['Content-Type']
-date = response.headers['Date']
 
-with open('goodbye.jpg', 'wb') as f:
-    f.write(response.content)
-
-import json
-data_about_image = {
-'Content-Length': content_length,
-'Content-Type': content_type,
-'Date': date
-}
-
-with open("data_about_image.json", "w") as json_file:
-    json.dump(data_about_image, json_file, indent=4)
+loading_cat_photo(text, url)
 
 
 import configparser
 config = configparser.ConfigParser()
 config.read('settings.ini')
 yd_token = config['tokens']['ya_disc']
+
+def create_folder(url_create_folder, params, headers):
+    response = requests.put(url_create_folder,
+                            params=params,
+                            headers=headers)
 
 url_create_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
 params = {
@@ -36,20 +43,24 @@ params = {
 headers = {
     'Authorization': f'OAuth {yd_token}'
 }
-response = requests.put(url_create_folder,
-                        params=params,
-                        headers=headers)
+
+create_folder(url_create_folder, params, headers)
+
+
+def upload_image(url_upload_link, params):
+    response_up = requests.get(url_upload_link,
+                               params=params,
+                               headers=headers)
+    print(response_up.json())
+    url_upload = response_up.json()['href']
+
+    with open('goodbye.jpg', 'rb') as f:
+        requests.put(url_upload, files={'file': f})
 
 url_upload_link = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 params = {
     'path': 'PD-FPY-SPD-130/goodbye.jpg',
     'overwrite': 'true'
 }
-response_up = requests.get(url_upload_link,
-                        params=params,
-                        headers=headers)
-print(response_up.json())
-url_upload = response_up.json()['href']
 
-with open('goodbye.jpg', 'rb') as f:
-    requests.put(url_upload, files={'file': f})
+upload_image(url_upload_link, params)
